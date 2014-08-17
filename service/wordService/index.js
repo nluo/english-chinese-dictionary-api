97edf9376dddfe83efcafe9161b825c734ee3d8b
Nick Luo <nick.luo@me.com>
2014-08-17 23:56:17 +1000

    Updated package.json

--- service/wordService/index.js
+++ service/wordService/index.js
@@ -1,9 +1,12 @@
 
 function getWord(req, res, tokens) {
 	console.log(tokens);
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	var result = tokens.word
-  	res.end(result);
+	// res.writeHead(200, {'Content-Type': 'textjson'});
+	var result = {};
+	result.word = tokens.word;
+	result.okay = " have a nice day";
+
+  	res.end(JSON.stringify(result));
 }
 
 module.exports = {
